4a2c2da778b452dd89c05215c50dbbf3d693637e
Satish Prem Anand <satishprem.anand2022@vitstudent.ac.in>
2024-04-08 19:17:30 +0530

    Revert "Changes Made:"
    This reverts commit 3606789a44847687ef0cf5fb7516680d31b04bf9.

--- pra/src/components/Quiz.js
+++
@@ -1,5 +0,0 @@
-function Quiz() {
-  return <div></div>;
-}
-
-export default Quiz;
--- pra/src/pages/GetStarted.js
+++
@@ -1,8 +0,0 @@
-import Card from "../components/Card";
-import Header from "../components/Header";
-
-function GetStarted() {
-  return <Header />;
-}
-
-export default GetStarted;
