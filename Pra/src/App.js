3af035911cbca157cd02a0f95d3a8a9d5b12d554
Sumit-Paudell <sumit.paudel2022@vitstudent.ac.in>
2024-04-09 22:30:20 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Pra/package-lock.json
    	modified:   Pra/package.json
    	modified:   Pra/src/App.js
    	modified:   Pra/src/pages/AdminDashboard.js
    	new file:   Pra/src/pages/Dashboard.js

--- Pra/src/App.js
+++ Pra/src/App.js
@@ -17,8 +17,9 @@ import ChiefWardenDashboard from "./pages/ChiefWardenDashboard";
 import WardenDashboard from "./pages/WardenDashboard";
 import AdminDashboard from "./pages/AdminDashboard";
 import Appointment from "./components/AppointmentForm";
+import Dashboard from "./pages/Dashboard";
 
-const DashboardPage = () => <h1>Dashboard Page</h1>;
+// const DashboardPage = () => <h1>Dashboard Page</h1>;
 const ProfilePage = () => <h1>Profile Page</h1>;
 const SettingsPage = () => <h1>Settings Page</h1>;
 
@@ -34,7 +35,7 @@ function App() {
         <Route path="/student" element={<StudentDashboard />} />
         <Route path="/warden" element={<WardenDashboard />} />
         <Route path="/admin" element={<AdminDashboard />}>
-          <Route path="/admin/dashboard" element={<DashboardPage />} />
+          <Route path="/admin/dashboard" element={<Dashboard />} />
           <Route path="/admin/appointment" element={<Appointment />} />
           <Route path="/admin/settings" element={<SettingsPage />} />
         </Route>
---
+++ Pra/src/pages/Dashboard.js
@@ -0,0 +1,46 @@
+import BarChart from "../components/BarChart";
+import DashboardCard from "../components/DashboardCard";
+
+function Dashboard() {
+  const docData = {
+    doctors: 100,
+    availableDoctors: 60,
+    occupiedDoctors: 40,
+    registeredPatients: 200,
+    ongoingTreatments: 120,
+    completedTreatments: 80,
+  };
+
+  const data = {
+    A: 100,
+    B: 120,
+    C: 80,
+    D: 150,
+    E: 90,
+    F: 110,
+    G: 130,
+    H: 140,
+    I: 70,
+    J: 100,
+    K: 120,
+    L: 80,
+    M: 150,
+    N: 90,
+    O: 110,
+  };
+
+  return (
+    <div className="w-full">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+        {" "}
+        {/* Added grid classes */}
+        {Object.entries(docData).map(([key, value], index) => (
+          <DashboardCard key={index} title={key} value={value} /> // Use DashboardCard
+        ))}
+      </div>
+      <div className="p-6">{/* <BarChart data={data} /> Use BarChart */}</div>
+    </div>
+  );
+}
+
+export default Dashboard;
